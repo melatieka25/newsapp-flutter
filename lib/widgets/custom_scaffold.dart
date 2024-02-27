d424003573ccc99d292ee415ba49b201871b3b25
melatieka25 <melatieka25@gmail.com>
2024-02-27 18:23:42 +0700

    refactor code

--- lib/widgets/custom_scaffold.dart
+++ lib/widgets/custom_scaffold.dart
@@ -12,33 +12,44 @@ class CustomScaffold extends StatelessWidget {
         child: Stack(
           children: [
             body,
-            Card(
-              margin: const EdgeInsets.all(0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 16.0),
-                    child: Text(
-                      'N',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                  ),
-                ],
-              ),
-              shape: const BeveledRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(16.0),
-                ),
-              ),
-            ),
+            _buildCustomAppBar(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _buildCustomAppBar extends StatelessWidget {
+  const _buildCustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Text(
+              'N',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+        ],
+      ),
+      shape: const BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(16.0),
         ),
       ),
     );
